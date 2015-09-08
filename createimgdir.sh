9406d5bffdc71219f2c4f407fbe0db7f1de32dc5
Alfie Pates <alfie.pates@outlook.com>
2015-09-08 10:09:12 +0100

    added createimgdir.sh

---
+++ createimgdir.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# alfiepates 2015
+# license: CC0 1.0
+# createimgdir.sh
+# this script creates "img" directories for a folder of markdown (.md) files, ignoring other files.
+
+mkdir -p img
+for file in $( ls ); do
+  if [ ${file: -3} == ".md" ]; then
+    mkdir ./img/`basename $file .md`
+  fi
+done
